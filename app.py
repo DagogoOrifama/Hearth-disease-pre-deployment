7bb98be5d98de84089442097865fc9f485c549d6
Dagogo Orifama <47601731+DagogoOrifama@users.noreply.github.com>
2024-04-21 19:35:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,60 +1,35 @@
-# https://youtu.be/l3QVYnMD128
-"""
-Application that predicts heart disease percentage in the population of a town
-based on the number of bikers and smokers. 
-
-Trained on the data set of percentage of people biking 
-to work each day, the percentage of people smoking, and the percentage of 
-people with heart disease in an imaginary sample of 500 towns.
-
-"""
-
-
 import numpy as np
 from flask import Flask, request, render_template
-import pickle
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
 
-#Create an app object using the Flask class. 
+# Create a Flask application
 app = Flask(__name__)
 
-#Load the trained model. (Pickle file)
-model = pickle.load(open('models/lungcan_model.h5', 'rb'))
+# Load the pre-trained model
+model = load_model('models/lungcan_model.h5')
 
-#Define the route to be home. 
-#The decorator below links the relative route of the URL to the function it is decorating.
-#Here, home function is with '/', our root directory. 
-#Running the app sends us to index.html.
-#Note that render_template means it looks for the file in the templates folder. 
-
-#use the route() decorator to tell Flask what URL should trigger our function.
 @app.route('/')
 def home():
     return render_template('index.html')
 
-#You can use the methods argument of the route() decorator to handle different HTTP methods.
-#GET: A GET message is send, and the server returns data
-#POST: Used to send HTML form data to the server.
-#Add Post method to the decorator to allow for form submission. 
-#Redirect to /predict page with the output
-@app.route('/predict',methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
-
-    int_features = [float(x) for x in request.form.values()] #Convert string inputs to float.
-    features = [np.array(int_features)]  #Convert to the form [[a, b]] for input to the model
-    prediction = model.predict(features)  # features Must be in the form [[a, b]]
-
-    output = round(prediction[0], 2)
-
-    return render_template('index.html', prediction_text='Percent with heart disease is {}'.format(output))
-
-
-#When the Python interpreter reads a source file, it first defines a few special variables. 
-#For now, we care about the __name__ variable.
-#If we execute our code in the main program, like in our case here, it assigns
-# __main__ as the name (__name__). 
-#So if we want to run our code right here, we can check if __name__ == __main__
-#if so, execute it here. 
-#If we import this file (module) to another file then __name__ == app (which is the name of this python file).
+    # Retrieve the file from post request
+    img_file = request.files['file']
+    
+    if img_file:
+        # Preprocess the image
+        img = image.load_img(img_file, target_size=(224, 224))
+        img_array = image.img_to_array(img)
+        img_array = np.expand_dims(img_array, axis=0) / 255.0
+        
+        # Make prediction
+        prediction = model.predict(img_array)
+        predicted_class = np.argmax(prediction, axis=1)
+        predicted_class_name = ['Benign', 'Malignant', 'Normal'][predicted_class[0]]
+        
+        return render_template('index.html', prediction_text=f'Predicted Class: {predicted_class_name}')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
